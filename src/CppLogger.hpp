48154d271776df4b1a5a1ffd670d7bc3b58f309d
Gleb Zolotov <GZolotov@inno.tech>
2022-05-31 22:11:19 +0300

    add_primitive_structure

--- src/CppLogger.hpp
+++ src/CppLogger.hpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <map>
+#include <thread>
 #include "boundedbuffer.hpp"
 
 namespace LogLevel {
@@ -21,34 +23,72 @@ namespace LogLevel {
     }
 };
 
-template<class T>
-class StructMsg {
-    private:
-        LogLevel::Level log_level;
-        T struct_msg;
-    public:
-        StructMsg(const T & in_msg, LogLevel::Level ll): struct_msg(in_msg), log_level(ll) {};
-        StructMsg & operator=(StructMsg&& rhs) { std::swap(struct_msg, rhs.struct_msg); log_level = rhs.log_level; return *this; };
-        std::string serialize() { struct_msg.set_level(log_level); return struct_msg.serialize(); };
-        ~StructMsg() {};
+std::string get_iso_time() {
+    auto timepoint = std::chrono::system_clock::now();
+    auto coarse = std::chrono::system_clock::to_time_t(timepoint);
+    auto fine = std::chrono::time_point_cast<std::chrono::milliseconds>(timepoint);
+
+    char buffer[sizeof "9999-12-31T23:59:59.999Z"];
+    std::snprintf(buffer + std::strftime(buffer, sizeof buffer - 4,
+                                         "%FT%T.", std::localtime(&coarse)),
+                  5, "%03luZ", fine.time_since_epoch().count() % 1000);
+    return buffer;
 };
 
-template<class T>
-class BaseOutput {
-    protected:
-        bounded_buffer< StructMsg<T> > & buffer;
-        virtual void send_msg(std::string) = 0;
-        std::atomic
+class StructMsg {
+    private:
+        std::string msg;
+        std::string rqId;
+        LogLevel::Level log_level;
     public:
-        BaseOutput(bounded_buffer< StructMsg<T> > & in_buf): buffer(in_buf) {};
-        void work() {
-            while()
+        StructMsg(std::string st=std::string(), LogLevel::Level l=LogLevel::INFO): msg(st), log_level(l) {}
+        void set_msg(std::string && rhs){ msg = rhs; }
+        void set_level(LogLevel::Level l){ log_level = l; }
+        void set_rqId(const std::string & in){ rqId = in; }
+        std::string serialize() { 
+            return "{\"level\": \"" + LogLevel::to_str(log_level) + "\", \"time\": \"" + get_iso_time() + "\", \"rqId\": \"" + rqId + "\", \"message\": \"" + msg + "\"}"; 
         }
+        ~StructMsg(){}
+};
 
+class BaseOutputter {
+    protected:
+        bounded_buffer< StructMsg & > & buffer;
+        virtual void write_msg(std::string) = 0;
+        std::atomic<bool> & is_worked;
+    public:
+        BaseOutputter(bounded_buffer< StructMsg & > & in_buf, std::atomic<bool> & in_a): buffer(in_buf), is_worked(in_a) {}
+        void work() {
+            while(is_worked.load()) {
+                StructMsg * msg;
+                buffer.pop_back(msg);
+                write_msg(msg->serialize());
+            }
+        }
+};
 
-}
+class ConsoleOutputter: public BaseOutputter {
+    protected:
+        void write_msg(std::string msg) override { std::cout << msg + "\n"; }
+};
+
+class PrimitiveMsg {
+    private:
+        std::string msg;
+        LogLevel::Level log_level;
+        std::thread::id thr_id;
+        std::atomic<bool> is_ready;
+    public:
+        PrimitiveMsg(std::string && rhs, LogLevel::Level l, std::thread::id t): msg(rhs), log_level(l), thr_id(t), is_ready(false) {}
+};
 
 class CppLogger {
+    private:
+        std::map<std::thread::id, StructMsg> log_templs;
+        bounded_buffer< StructMsg & > buffer;
+        BaseOutputter & out;
     public:
-        CppLogger() {};
+        CppLogger(std::string format) {};
+        void info_msg(std::string && rhs) { if }
+        void put_msg(StructMsg)
 };
