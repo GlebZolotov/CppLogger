6d32ff79bfd0efa344f55e5403db6ce7703e4f6b
Gleb Zolotov <GZolotov@inno.tech>
2022-09-09 18:21:30 +0300

    add_level_and_file

--- src/CppLogger.hpp
+++ src/CppLogger.hpp
@@ -7,10 +7,12 @@
 #include <algorithm>
 #include <regex>
 #include <mutex>
+#include <cstdio>
 #include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
 
 namespace LogLevel {
-    enum Level { TRACE, DEBUG, INFO, WARNING, ERROR, CRITICAL };
+    enum Level { UNKNOWN, TRACE, DEBUG, INFO, WARNING, ERROR, CRITICAL };
     std::string to_str(Level a) {
         switch (a) {
         case TRACE:
@@ -26,10 +28,35 @@ namespace LogLevel {
         case CRITICAL:
             return std::string("CRITICAL");
         default:
-            return std::string("IT IS NON_FULL SWITCH!!!");
+            return std::string("UNKNOWN");
         }
     }
+    Level from_str(const std::string & a) {
+        if (a == "TRACE") 
+            return TRACE;
+        if (a == "DEBUG")
+            return DEBUG;
+        if (a == "INFO")
+            return INFO;
+        if (a == "WARNING")
+            return WARNING;
+        if (a == "ERROR")
+            return ERROR;
+        if (a == "CRITICAL")
+            return CRITICAL;
+        return UNKNOWN;
+    }
 }
+
+struct Options {
+    std::string output_type;
+    std::string host;
+    std::string port;
+    std::string path_to_file;
+    std::string path_to_template;
+    std::string level;
+    size_t buf_size;
+};
 
 class PrimitiveMsg {
     private:
@@ -168,7 +195,7 @@ class BaseOutputter {
             buffer(in_buf), 
             is_worked(in_a),
             log_templs(l) {}
-        virtual std::pair<bool, std::string> init(std::string host, std::string port) {}
+        virtual std::pair<bool, std::string> init(Options settings) {}
         void work() {
             while(is_worked.load() || buffer.cur_count() > 0) {
                 PrimitiveMsg * msg;
@@ -198,32 +225,82 @@ class HttpOutputter: public BaseOutputter {
         boost::asio::io_service service;
         boost::asio::ip::tcp::endpoint ep;
         boost::asio::ip::tcp::socket sock;
+
+        std::string host;
+        std::string path_to_file;
+        std::ofstream fout;
+
+        boost::asio::streambuf request;
+
+        bool is_success_last;
     protected:
-        void write_msg(const std::string & msg) override {
-            boost::asio::streambuf request;
-            std::ostream request_stream(&request);
-
-            request_stream << "POST / HTTP/1.1\r\n";
-            request_stream << "Host: localhost:24224\r\n";
-            request_stream << "User-Agent: curl/7.68.0\r\n";
-            request_stream << "Accept: */*\r\n";
-            request_stream << "Content-Type: application/json\r\n";
-            request_stream << "Content-Length: " << msg.length() << "\r\n";    
-            request_stream << "Connection: close\r\n\r\n";  //NOTE THE Double line feed
-            request_stream << msg;
-
+        std::pair<bool, std::string> send_message() {
+            std::pair<bool, std::string> res(false, "");
             boost::asio::write(sock, request);
+            sock.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+
+            // read for max 5s
+            boost::asio::steady_timer timer(io_context, std::chrono::seconds(5));
+            timer.async_wait([&](boost::system::error_code ec) { sock.cancel(); });
+
+            std::string reply_buf;
+            boost::system::error_code reply_ec;
+            boost::asio::async_read(sock, boost::asio::dynamic_buffer(res.second, 2048),
+                [&](boost::system::error_code ec, size_t) { timer.cancel(); reply_ec = ec; });
+
+            io_context.run();
+
+            if (!reply_ec || reply_ec == boost::asio::error::eof) {
+                std::cout << "Response:\n" << res.second << std::endl;
+                res.first = true;
+            }
+            return res;
+        }
+        void write_msg(const std::string & msg) override {
+            if (is_success_last) {
+                std::ostream request_stream(&request);
+
+                request_stream << "POST / HTTP/1.1\r\n";
+                request_stream << "Host: " << host << "\r\n";
+                request_stream << "User-Agent: curl/7.68.0\r\n";
+                request_stream << "Accept: */*\r\n";
+                request_stream << "Content-Type: application/json\r\n";
+                request_stream << "Content-Length: " << msg.length() << "\r\n";    
+                request_stream << "Connection: close\r\n\r\n";  //NOTE THE Double line feed
+                request_stream << msg;
+            }
+            std::pair<bool, std::string> resp = send_message();
+            if (resp.first) {
+                if (is_success_last) return;
+                is_success_last = true;
+                fout.close();
+                std::ifstream fin(path_to_file);
+                std::string new_msg;
+                while(std::getline(fin, new_msg)) {
+                    write_msg(new_msg);
+                }
+                fin.close();
+                std::remove(path_to_file.c_str());
+            } else {
+                if (is_success_last) {
+                    is_success_last = false;
+                    fout = std::ofstream(path_to_file);
+                } else {
+                    fout << msg;
+                }
+            }
         }
     public:
         HttpOutputter(
             bounded_buffer<PrimitiveMsg *> & in_buf, 
             std::atomic<bool> & in_a, 
             ThrTempls &l
-        ): BaseOutputter(in_buf, in_a, l), sock(service) {}
-        std::pair<bool, std::string> init(std::string host, std::string port) override {
+        ): BaseOutputter(in_buf, in_a, l), sock(service), is_success_last(true) {}
+        std::pair<bool, std::string> init(Options settings) override {
             try{
+                path_to_file = settings.path_to_file;
                 boost::asio::ip::tcp::resolver resolver(service);
-                boost::asio::ip::tcp::resolver::query query(host, port);
+                boost::asio::ip::tcp::resolver::query query(settings.host, settings.port);
                 boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
                 ep = iter->endpoint();
                 sock.connect(ep);
@@ -253,22 +330,29 @@ class CppLogger {
         StructMsg base_format;
         std::thread output_thr;
         std::mutex m;
+        LogLevel::Level level;
 
         static size_t const default_capacity = 50;
 
-        CppLogger(): 
-            buffer(default_capacity), 
-            is_work(true), 
-            out(std::make_unique<HttpOutputter>(buffer, is_work, log_templs)), 
-            output_thr(&BaseOutputter::work, out.get()) 
+        CppLogger(Options settings): 
+            buffer(settings.buf_size), 
+            is_work(true) 
         {
             std::cerr << "Create CppLogger instance" << std::endl;
-            std::pair<bool, std::string> res = out->init("localhost", "24224");
+            std::transform(settings.level.begin(), settings.level.end(), settings.level.begin(), ::toupper);
+            level = LogLevel::from_str(settings.level);
+            if (settings.output_type == "http")
+                out = std::make_unique<HttpOutputter>(buffer, is_work, log_templs);
+            else if (settings.output_type == "console") 
+                out = std::make_unique<ConsoleOutputter>(buffer, is_work, log_templs); 
+
+            std::pair<bool, std::string> res = out->init(settings);
             if (!res.first) {
                 std::cerr << "Failed to init logger: " << res.second << std::endl;
                 is_work.store(false);
                 return;
             }
+            output_thr = std::thread(&BaseOutputter::work, out.get());
             reg_thread();
         }
         ~CppLogger() { 
@@ -291,8 +375,8 @@ class CppLogger {
                 el.second->set_format(base_format);
         }
     public:
-        static CppLogger & get_logger() {
-            static CppLogger logger;
+        static CppLogger & get_logger(Options * settings = nullptr) {
+            static CppLogger logger(*settings);
             return logger;
         }
         CppLogger(const CppLogger&) = delete;
@@ -328,21 +412,27 @@ class CppLogger {
         }
 
         void trace_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::TRACE));
+            if (level <= LogLevel::TRACE)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::TRACE));
         }
         void debug_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::DEBUG));
+            if (level <= LogLevel::DEBUG)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::DEBUG));
         }
         void info_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::INFO));
+            if (level <= LogLevel::INFO)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::INFO));
         }
         void warning_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::WARNING));
+            if (level <= LogLevel::WARNING)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::WARNING));
         }
         void error_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::ERROR));
+            if (level <= LogLevel::ERROR)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::ERROR));
         }
         void critical_msg(const std::string & msg) {
-            buffer.push_front(new PrimitiveMsg(msg, LogLevel::CRITICAL));
+            if (level <= LogLevel::CRITICAL)
+                buffer.push_front(new PrimitiveMsg(msg, LogLevel::CRITICAL));
         }
 };
